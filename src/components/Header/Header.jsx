2d603ea7eb0dc8322ccc1abc2111f77a424bd669
prakharcodehere <dubeyprakhar13@gmail.com>
2024-04-05 23:47:23 +0530

    carousel fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,9 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Header.css';
+import Carousel1 from "../../assets/images/carousel1.jpg"
+import Carousel2 from "../../assets/images/carusel2.jpg"
+import Carousel3 from "../../assets/images/carousel3.jpg"
 
 const Header = () => {
   const settings = {
@@ -16,17 +19,18 @@ const Header = () => {
 
   return (
     <div className="header-container">
-      <Slider {...settings}>
+      <Slider {...settings} className='slider-wrapper'>
         <div className='swiper'>
           <h4>book your <br /> medical center ONLINE</h4>
-        
+          <img src={Carousel1} alt="carousel" />
         </div>
         <div className='swiper'>
+        <img src={Carousel2} alt="carousel" />
          
         </div>
         <div className='swiper'>
         
-        <img src="../../assets/images/carousel1.jpg" alt=""  className='swiper-img' />
+        <img src={Carousel3} alt=""  className='swiper-img' />
         </div>
         
       </Slider>
